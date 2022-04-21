2e144c1e193c98ad68322eeee3745bd3ad072619
Nelly Nikitina <100719863+nelllyai@users.noreply.github.com>
2022-04-21 23:41:57 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,3 +7,21 @@ TEST(task1, test1) {
     int result = gen.getPrefixSize();
     EXPECT_EQ(2, result);
 }
+
+TEST(task2, test2) {
+    MarkovGenerator gen = MarkovGenerator("test.txt", NPREF);
+    int result = gen.getSuffixSize("corpses", "buried");
+    EXPECT_EQ(1, result);
+}
+
+TEST(task3, test3) {
+    MarkovGenerator gen = MarkovGenerator("test.txt", NPREF);
+    int result = gen.getPrefixSize();
+    EXPECT_EQ(2, result);
+}
+
+TEST(task4, test4) {
+    MarkovGenerator gen = MarkovGenerator("test.txt", NPREF);
+    int result = gen.getPrefixSize();
+    EXPECT_EQ(2, result);
+}
