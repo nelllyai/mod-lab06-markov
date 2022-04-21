2b1ac61d1e028846f3abce7368cbe18607336be7
Nelly Nikitina <100719863+nelllyai@users.noreply.github.com>
2022-04-21 23:02:39 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -2,60 +2,60 @@
 #include "textgen.h"
 
 MarkovGenerator::MarkovGenerator(std::string path, int preflen) {
-   prefixSize = preflen;
-   ifstream file(path);
-   string s;
-   for (file >> s; !file.eof(); file >> s)
-   {
-      words.push_back(s);
-   }
+    prefixSize = preflen;
+    ifstream file(path);
+    string s;
+    for (file >> s; !file.eof(); file >> s)
+    {
+        words.push_back(s);
+    }
 
-   for (int i = 0; i < words.size() - prefixSize + 1; i++) {
-      prefix aFewWords;
-      vector<string> suffixes;
+    for (int i = 0; i < words.size() - prefixSize + 1; i++) {
+        prefix aFewWords;
+        vector<string> suffixes;
 
-      for (int j = 0; j < prefixSize; j++) {
-         aFewWords.push_back(words[i + j]);
-      }
+        for (int j = 0; j < prefixSize; j++) {
+            aFewWords.push_back(words[i + j]);
+        }
 
-      for (int j = 0; j < words.size() - prefixSize; j++) {
-         bool toSave = true;
-         for (int k = 0; k < prefixSize; k++) {
-            if (words[i + k] != words[j + k])
-               toSave = false;
-         }
-         if (toSave)
+        for (int j = 0; j < words.size() - prefixSize; j++) {
+            bool toSave = true;
+            for (int k = 0; k < prefixSize; k++) {
+                if (words[i + k] != words[j + k])
+                    toSave = false;
+        }
+        if (toSave)
             suffixes.push_back(words[j + prefixSize]);
-      }
+        }
 
-      statetab.insert(make_pair(aFewWords, suffixes));
-   }
+    statetab.insert(make_pair(aFewWords, suffixes));
+    }
 }
 
 string MarkovGenerator::getText(int wordsamount) {
-   prefix currentPrefixes;
-   for (int i = 0; i < prefixSize; i++) {
-      currentPrefixes.push_back(words[i]);
-   }
-   srand(time(NULL));
+    prefix currentPrefixes;
+    for (int i = 0; i < prefixSize; i++) {
+        currentPrefixes.push_back(words[i]);
+    }
+    srand(time(NULL));
 
-   string result = currentPrefixes[0] + " " + currentPrefixes[1] + " ";
+    string result = currentPrefixes[0] + " " + currentPrefixes[1] + " ";
 
-   for (int i = prefixSize; i < wordsamount - prefixSize; i++) {
-      vector <string> currentSuffix = statetab.at(currentPrefixes);
+    for (int i = prefixSize; i < wordsamount - prefixSize; i++) {
+        vector <string> currentSuffix = statetab.at(currentPrefixes);
 
-      if (currentSuffix.size() == 0)
-         break;
+        if (currentSuffix.size() == 0)
+            break;
 
-      int index = rand_r() % currentSuffix.size();
-      result += currentSuffix[index] + " ";
-      currentPrefixes.erase(currentPrefixes.begin());
-      currentPrefixes.push_back(currentSuffix[index]);
-   }
+        int index = rand() % currentSuffix.size();
+        result += currentSuffix[index] + " ";
+        currentPrefixes.erase(currentPrefixes.begin());
+        currentPrefixes.push_back(currentSuffix[index]);
+    }
 
-   return result;
+    return result;
 }
 
 int MarkovGenerator::getPrefixSize() {
-   return prefixSize;
+    return prefixSize;
 }
